f44c1f49a3fd71d926c6d6883b050c941ddface1
Deosrc <deosrc@g-de.co.uk>
2026-02-23 23:25:59 +0000

    Split device tracker into last known position and live position

--- custom_components/pitpat/device_tracker.py
+++ custom_components/pitpat/device_tracker.py
@@ -1,5 +1,6 @@
+from dataclasses import dataclass
 import logging
-from typing import Any, Dict
+from typing import Any, Callable, Dict
 
 import dateutil
 from homeassistant.core import HomeAssistant
@@ -22,8 +23,46 @@ from .coordinator import PitPatDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
+
 def _get_monitor(data: dict) -> dict:
-    return data.get('monitor_details', {}).get('Value', {}).get('Monitor', {})
+    return data.get('monitor_details', {}).get('Value', {})
+
+def _get_monitor_position(data: dict) -> dict:
+    return _get_monitor(data).get('Monitor', {}).get('LastKnownPosition', {}).get('Value', {})
+
+def _is_tracking_live(data: dict) -> bool:
+    return _get_monitor(data).get('GpsSynchronisationState', 0) == 3
+
+@dataclass(frozen=True, kw_only=True)
+class PitPatTrackerEntityDescription(TrackerEntityDescription):
+    available_fn: Callable[[dict], bool | None] = lambda data: True
+    latitude_fn: Callable[[dict], float | None]
+    longitude_fn: Callable[[dict], float | None]
+    accuracy_fn: Callable[[dict], float]
+    attributes_fn: Callable[[dict], dict | None] = None
+
+ENTITY_DESCRIPTIONS = [
+    PitPatTrackerEntityDescription(
+        key='last_known_position',
+        translation_key='last_known_position',
+        icon="mdi:dog",
+        latitude_fn=lambda data: float(_get_monitor_position(data).get('Latitude')),
+        longitude_fn=lambda data: float(_get_monitor_position(data).get('Longitude')),
+        accuracy_fn=lambda data: float(_get_monitor_position(data).get('Accuracy', {}).get('Metres')),
+        attributes_fn=lambda data: {
+            "last_updated": dateutil.parser.parse(_get_monitor_position(data).get('DataTime'))
+        }
+    ),
+    PitPatTrackerEntityDescription(
+        key='live_position',
+        translation_key='live_position',
+        icon="mdi:dog",
+        available_fn=lambda data: _is_tracking_live(data),
+        latitude_fn=lambda data: float(_get_monitor_position(data).get('Latitude')),
+        longitude_fn=lambda data: float(_get_monitor_position(data).get('Longitude')),
+        accuracy_fn=lambda data: float(_get_monitor_position(data).get('Accuracy', {}).get('Metres')),
+    )
+]
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities):
     """Add the Entities from the config."""
@@ -31,21 +70,19 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, asyn
     sensors = []
 
     for dog_id in coordinator.dogs.keys():
-        sensors.append(PitPatDogDeviceTrackerEntity(coordinator, dog_id))
+        for description in ENTITY_DESCRIPTIONS:
+            sensors.append(PitPatDogDeviceTrackerEntity(coordinator, dog_id, description))
 
     async_add_entities(sensors, True)
 
 class PitPatDogDeviceTrackerEntity(CoordinatorEntity[PitPatDataUpdateCoordinator], TrackerEntity):
 
-    def __init__(self, coordinator: PitPatDataUpdateCoordinator, dog_id: str):
+    _attr_has_entity_name = True
+
+    def __init__(self, coordinator: PitPatDataUpdateCoordinator, dog_id: str, description: PitPatTrackerEntityDescription):
         CoordinatorEntity.__init__(self, coordinator)
         self._dog_id = dog_id
-
-        self.entity_description = TrackerEntityDescription(
-            key='tracker',
-            translation_key='tracker',
-            icon="mdi:dog"
-        )
+        self.entity_description = description
 
         # Required for HA 2022.7
         self.coordinator_context = object()
@@ -55,37 +92,54 @@ class PitPatDogDeviceTrackerEntity(CoordinatorEntity[PitPatDataUpdateCoordinator
         return f'{self._dog_id}-{self.entity_description.key}'
 
     @property
+    def description(self) -> PitPatTrackerEntityDescription:
+        return self.entity_description
+
+    @property
     def data(self):
         return self.coordinator.dogs.get(self._dog_id)
 
     @property
-    def latitude(self):
+    def available(self) -> bool:
         try:
-            return _get_monitor(self.data).get('LastKnownPosition', {}).get('Value', {}).get('Latitude')
+            return self.description.available_fn(self.data)
+        except Exception as e:
+            raise ValueError(f"Unable to get availability value for {self.entity_description.key} device tracker entity for dog id {self._dog_id}") from e
+
+    @property
+    def latitude(self) -> float | None:
+        try:
+            return self.description.latitude_fn(self.data)
         except Exception as e:
             raise ValueError(f"Unable to get latitude value for {self.entity_description.key} device tracker entity for dog id {self._dog_id}") from e
 
     @property
-    def longitude(self):
+    def longitude(self) -> float | None:
         try:
-            return _get_monitor(self.data).get('LastKnownPosition', {}).get('Value', {}).get('Longitude')
+            return self.description.longitude_fn(self.data)
         except Exception as e:
             raise ValueError(f"Unable to get longitude value for {self.entity_description.key} device tracker entity for dog id {self._dog_id}") from e
 
     @property
-    def location_accuracy(self):
+    def location_accuracy(self) -> float | None:
         try:
-            return _get_monitor(self.data).get('LastKnownPosition', {}).get('Value', {}).get('Accuracy', {}).get('Metres')
+            return self.description.accuracy_fn(self.data)
         except Exception as e:
             raise ValueError(f"Unable to get accuracy value for {self.entity_description.key} device tracker entity for dog id {self._dog_id}") from e
 
     @property
+    def base_attributes(self) -> Dict[str, Any] | None:
+        return {
+            "dog_id": self._dog_id,
+        }
+
+    @property
     def extra_state_attributes(self) -> Dict[str, Any] | None:
         try:
-            return {
-                "dog_id": self._dog_id,
-                "last_updated": dateutil.parser.parse(_get_monitor(self.data).get('LastKnownPosition', {}).get('Value', {}).get('DataTime'))
-            }
+            attributes = self.base_attributes
+            if self.description.attributes_fn:
+                attributes = {**attributes, **self.description.attributes_fn(self.data)}
+            return attributes
         except Exception as e:
             raise ValueError(f"Unable to get attributes for {self.entity_description.key} sensor entity for dog id {self._dog_id}") from e
 
