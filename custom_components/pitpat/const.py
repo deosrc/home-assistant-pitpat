37c9d1fb080e584e8367e80daadd491adfaf605d
deosrc <deosrc@g-de.co.uk>
2026-02-23 22:40:38 +0000

    Add GPS tracker model info to device (#40)
    Currently on

--- custom_components/pitpat/const.py
+++ custom_components/pitpat/const.py
@@ -1,3 +1,6 @@
+from typing import Dict
+
+
 DOMAIN = "pitpat"
 MANUFACTURER = "PitPat"
 
@@ -6,3 +9,7 @@ OPTIONS_KEY_UPDATE_INTERVAL = "update_interval"
 DATA_KEY_COORDINATOR = "coordinator"
 
 UPDATE_INTERVAL_DEFAULT = 5
+
+DEVICE_MODEL_MAP: Dict[int, str] = {
+    6: 'GPS Tracker'
+}
